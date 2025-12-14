bf88daf3212947bda1326e59644db702447f4812
Serialong Sebele <sebeleserialong4@gmail.com>
2025-12-14 21:29:28 +0200

    Add files via upload
    This script proves a successful installation and validates enterprise configuration standards.

---
+++ Installation/post-install-validation.sql
@@ -0,0 +1,19 @@
+-- Confirm server version 
+SELECT version();
+
+-- Confirm character set
+SHOW VARIABLES LIKE 'character_set%';
+
+-- Confirm data directory
+SHOW VARIABLES LIKE 'datadir';
+
+-- Confirm binary logging
+SHOW VARIABLES LIKE 'log_bin';
+
+-- Confirm time zone
+SHOW VARIABLES LIKE 'time_zone';
+
+
+
+
+

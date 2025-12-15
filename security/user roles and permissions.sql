c0d855a4c97459be043660ca1106a89fc03df7d5
Serialong Sebele <sebeleserialong4@gmail.com>
2025-12-15 23:28:31 +0200

    Add files via upload

---
+++ security/user roles and permissions.sql
@@ -0,0 +1,41 @@
+-- User roles&permissions
+
+
+GRANT ALL PRIVILEGES
+ON drugservice.*
+TO dba_role;
+
+-- etl permissions
+GRANT SELECT, INSERT, UPDATE
+ON drugservice.drugs
+TO etl_role;
+
+GRANT SELECT, INSERT, UPDATE
+ON drugservice.facilities
+TO etl_role; -- This means they can load and clean data, they cannot drop tables, they cannot manage users
+
+-- analyst role
+GRANT SELECT
+ON drugservice.drugs
+TO analyst_role;
+
+GRANT SELECT
+ON drugservice.facilities
+TO analyst_role; -- safe for reporting and no data modification
+
+-- creating users
+CREATE USER 'db_admin'@'%''localhost'
+IDENTIFIED BY 'StrongDBAPassword!';
+
+CREATE USER 'etl_user'@'localhost'
+IDENTIFIED BY 'StrongETLPassword!';
+
+CREATE USER 'analyst_user'@'localhost'
+IDENTIFIED BY 'StrongAnalystPassword!';
+
+-- assigning roles to users
+
+GRANT dba_role TO 'db_admin'@'%''localhost';
+GRANT etl_role TO 'etl_user'@'localhost';
+GRANT analyst_role TO 'analyst_user'@'localhost';
+
